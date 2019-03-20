f1e9715c900c3747aa92dffda6ef7a1c1cd2e979
dvilajeti01 <43216832+dvilajeti01@users.noreply.github.com>
2019-03-19 23:46:36 -0400

    Update Queue.cpp

--- Queue.cpp
+++ Queue.cpp
@@ -5,6 +5,7 @@
 //  Created by Daniel Vilajeti on 3/3/19.
 //  Copyright © 2019 Daniel Vilajeti. All rights reserved.
 //
+
 #include <iostream>
 #include <stdio.h>
 #include "Queue.h"
@@ -12,73 +13,74 @@
 
 using namespace std;
 
-Queue :: Queue()
+Queue::Queue()
 {
-    front = nullptr;
-    rear = nullptr;
-    num_items = 0;
+	front = nullptr;
+	rear = nullptr;
+	num_items = 0;
 }
 
 Queue::~Queue()
 {
-    clear();
+	clear();
 }
 
 void Queue::enqueue(int num)
 {
-    node *newNode = nullptr;
-    
-    newNode = new node;
-    
-    newNode->value = num;
-    newNode->next = nullptr;
-    
-    if(isEmpty())
-    {
-        front = newNode;
-        rear = newNode;
-    }
-    else
-    {
-        rear->next = newNode;
-        rear = newNode;
-    }
-    
-    num_items++;
+	node *newNode = nullptr;
+
+	newNode = new node;
+
+	newNode->value = num;
+	newNode->next = nullptr;
+
+	if (isEmpty())
+	{
+		front = newNode;
+		rear = newNode;
+	}
+	else
+	{
+		rear->next = newNode;
+		rear = newNode;
+	}
+
+	num_items++;
 }
 
 void Queue::dequeue(int &x)
 {
-    node *temp = nullptr;
-    
-    if(isEmpty())
-    {
-        cout<<"Queue is empty!\n";
-    }
-    else
-    {
-        x = front->value;
-        temp = front;
-        front = front->next;
-        delete temp;
-        
-        num_items--;
-        
-    }
+	node *temp = nullptr;
+
+	if (isEmpty())
+	{
+		cout << "Queue is empty!\n";
+	}
+	else
+	{
+		x = front->value;
+		temp = front;
+		front = front->next;
+		delete temp;
+
+		num_items--;
+
+	}
 }
 
 bool Queue::isEmpty()
 {
-    if (num_items > 0)
-        return false;
-    else
-        return true;
+	if (num_items > 0)
+		return false;
+	else
+		return true;
 }
 
 void Queue::clear()
 {
-    int value;
-    
-    while(!isEmpty())
-        dequeue(value);
+	int value;
+
+	while (!isEmpty())
+		dequeue(value);
 }
+
