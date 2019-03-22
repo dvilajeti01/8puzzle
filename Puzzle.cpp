7eec7c2132e2cb3aaef526ea4db8dffca6e68b3c
dvilajeti01 <43216832+dvilajeti01@users.noreply.github.com>
2019-03-22 14:40:57 -0400

    Update Puzzle.cpp

--- Puzzle.cpp
+++ Puzzle.cpp
@@ -1,86 +1,110 @@
 //
-//  Breadth_First_Search.cpp
+//  Puzzle.cpp
 //  AI_Project1
 //
-//  Created by Daniel Vilajeti on 3/3/19.
+//  Created by Daniel Vilajeti on 3/4/19.
 //  Copyright © 2019 Daniel Vilajeti. All rights reserved.
 //
 
-#include <iostream>
 #include <stdio.h>
-#include "Queue.h"
-
-
+#include "Puzzle.h"
+#include <iostream>
+#include <unordered_map>
 using namespace std;
 
-Queue::Queue()
+bool Puzzle::Goal_Test(int initial_state[3][3], int goal_state[3][3])
 {
-	front = nullptr;
-	rear = nullptr;
-	num_items = 0;
-}
-
-Queue::~Queue()
-{
-	clear();
-}
-
-void Queue::enqueue(int move[3][3])
-{
-	node *newNode = nullptr;
-
-	newNode = new node;
-
-	newNode->move = num;
-	newNode->next = nullptr;
-
-	if (isEmpty())
+	for (int i = 0; i < 3; i++)
 	{
-		front = newNode;
-		rear = newNode;
-	}
-	else
-	{
-		rear->next = newNode;
-		rear = newNode;
+		for (int j = 0; j < 3; j++)
+		{
+			if (initial_state[i][j] != goal_state[i][j])
+			{
+				return false;
+			}
+		}
 	}
 
-	num_items++;
+	return true;
 }
 
-void Queue::dequeue(int &x)
+void Puzzle::next_state(int current_state[3][3])
 {
-	node *temp = nullptr;
 
-	if (isEmpty())
+}
+
+void Puzzle::shuffle(int initial_state[3][3])
+{
+
+	unordered_map<int, int[3]> order;
+	int i_arr[3];
+	
+	//POPULATE ORDERED MAP WHICH REPRESENTS THE INDEXES OF A SHUFFLED 2-D ARRAY
+	for (int i = 0; i < 3; i++)
 	{
-		cout << "Queue is empty!\n";
+		rand_array(order[i_arr[i]]);
+
 	}
-	else
+
+	int temp[3][3];
+	//COPY INITIAL ARRAY ELEMENTS TO TO TEMP ARRAY
+	for (int i = 0; i < 3; i++)
 	{
-		x = front->value;
-		temp = front;
-		front = front->next;
-		delete temp;
-
-		num_items--;
-
+		for (int j = 0; j < 3; j++)
+		{
+			temp[i][j] = initial_state[i][j];
+		}
 	}
+
+	//SHUFFLE 2-D ARRAY
+	for (auto i = order.begin(); i != order.end(); i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			for (int k = 0; k < 3; k++)
+			{
+				initial_state[j][k] = temp[i->first][i->second[k]];
+			}
+			i++;
+		}
+	}
+
+
 }
 
-bool Queue::isEmpty()
+void Puzzle::rand_array(int real_arr[3])
 {
-	if (num_items > 0)
-		return false;
-	else
-		return true;
+	int count = 0;
+	int num;
+	bool exists = 0;
+
+	//INITIALIZES AN ARRAY WITH 3 DISTINCT RANDOM NUMBERS RANGING FROM 0-2(inclusive)
+	for (int i = 2; i >= 0; i--)
+	{
+		count = 0;
+		
+		while (count == 0)
+		{
+			num = rand() % 3;
+
+			for (int j = i + 1; j < 3; j++)
+			{
+				if (real_arr[j] == num)
+				{
+					exists = true;
+					break;
+				}
+				else
+					exists = false;
+			}
+
+			if (exists == false)
+			{
+				real_arr[i] = num;
+				count = 1;
+			}
+		}
+	}
+
+
 }
-
-void Queue::clear()
-{
-	int value;
-
-	while (!isEmpty())
-		dequeue(value);
-}
-
