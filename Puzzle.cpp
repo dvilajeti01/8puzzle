79796ab9e43400d72581353e7a5c7a06fe119e15
dvilajeti01 <43216832+dvilajeti01@users.noreply.github.com>
2019-03-22 14:39:12 -0400

    Update Puzzle.cpp

--- Puzzle.cpp
+++ Puzzle.cpp
@@ -1,86 +1,86 @@
 //
-//  Puzzle.cpp
+//  Breadth_First_Search.cpp
 //  AI_Project1
 //
-//  Created by Daniel Vilajeti on 3/4/19.
+//  Created by Daniel Vilajeti on 3/3/19.
 //  Copyright © 2019 Daniel Vilajeti. All rights reserved.
 //
 
-#include <stdio.h>
-#include "Puzzle.h"
 #include <iostream>
-#include <unordered_map>
+#include <stdio.h>
+#include "Queue.h"
+
+
 using namespace std;
 
-void Puzzle::shuffle(int initial_state[3][3])
+Queue::Queue()
 {
-
-	unordered_map<int, int[3]> order;
-	int i_arr[3];
-	
-	for (int i = 0; i < 3; i++)
-	{
-		rand_array(order[i_arr[i]]);
-
-	}
-
-	int temp[3][3];
-	//COPY INITIAL ARRAY ELEMENTS TO TO TEMP ARRAY
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			temp[i][j] = initial_state[i][j];
-		}
-	}
-
-	for (auto i = order.begin(); i != order.end(); i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			for (int k = 0; k < 3; k++)
-			{
-				initial_state[j][k] = temp[i->first][i->second[k]];
-			}
-			i++;
-		}
-	}
-
-
+	front = nullptr;
+	rear = nullptr;
+	num_items = 0;
 }
 
-void Puzzle::rand_array(int real_arr[3])
+Queue::~Queue()
 {
-	int count = 0;
-	int num;
-	bool exists = 0;
+	clear();
+}
 
-	for (int i = 2; i >= 0; i--)
+void Queue::enqueue(int move[3][3])
+{
+	node *newNode = nullptr;
+
+	newNode = new node;
+
+	newNode->move = num;
+	newNode->next = nullptr;
+
+	if (isEmpty())
 	{
-		count = 0;
-		
-		while (count == 0)
-		{
-			num = rand() % 3;
-
-			for (int j = i + 1; j < 3; j++)
-			{
-				if (real_arr[j] == num)
-				{
-					exists = true;
-					break;
-				}
-				else
-					exists = false;
-			}
-
-			if (exists == false)
-			{
-				real_arr[i] = num;
-				count = 1;
-			}
-		}
+		front = newNode;
+		rear = newNode;
+	}
+	else
+	{
+		rear->next = newNode;
+		rear = newNode;
 	}
 
-
+	num_items++;
 }
+
+void Queue::dequeue(int &x)
+{
+	node *temp = nullptr;
+
+	if (isEmpty())
+	{
+		cout << "Queue is empty!\n";
+	}
+	else
+	{
+		x = front->value;
+		temp = front;
+		front = front->next;
+		delete temp;
+
+		num_items--;
+
+	}
+}
+
+bool Queue::isEmpty()
+{
+	if (num_items > 0)
+		return false;
+	else
+		return true;
+}
+
+void Queue::clear()
+{
+	int value;
+
+	while (!isEmpty())
+		dequeue(value);
+}
+
