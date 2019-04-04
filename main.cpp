0e96435e7b4e4ba2290727d71302c5b541568429
dvilajeti01 <43216832+dvilajeti01@users.noreply.github.com>
2019-04-03 22:07:55 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 
 #include <iostream>
-#include <string>
+#include <chrono>
+#include "Puzzle.h"
 using namespace std;
 
 
-int main(int argc, const char * argv[]) {
-    
+int main() 
+{
+	using namespace chrono;
+	Puzzle test;
 
+	high_resolution_clock::time_point start_T = high_resolution_clock::now();
+	test.solve();
+	high_resolution_clock::time_point end_T = high_resolution_clock::now();
+	
+	cout << "The progrma took " << duration_cast<seconds>(end_T - start_T).count() << " seconds." << endl;
+
+	return 0;
 }
