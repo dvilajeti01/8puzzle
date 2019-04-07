4f7a24a12598395cdf7a8b1a0e11b2cddd0302ac
dvilajeti01 <43216832+dvilajeti01@users.noreply.github.com>
2019-04-06 20:18:50 -0400

    Update Queue.cpp

--- Queue.cpp
+++ Queue.cpp
@@ -3,7 +3,7 @@
 //  AI_Project1
 //
 //  Created by Daniel Vilajeti on 3/3/19.
-//  Copyright © 2019 Daniel Vilajeti. All rights reserved.
+//  Copyright 2019 Daniel Vilajeti. All rights reserved.
 //
 
 #include <iostream>
@@ -25,13 +25,21 @@ Queue::~Queue()
 	clear();
 }
 
-void Queue::enqueue(int num)
+void Queue::enqueue(int move[3][3], int **root_move, int row, int col)
 {
 	node *newNode = nullptr;
 
 	newNode = new node;
 
-	newNode->value = num;
+	memcpy(newNode->move,move,sizeof(newNode->move));
+
+	for (int i = 0; i < 3; i++)
+	{
+		memcpy(*(newNode->root_move + i), *(root_move + i), sizeof(*newNode->root_move));
+	}
+	
+	newNode->row = row;
+	newNode->col = col;
 	newNode->next = nullptr;
 
 	if (isEmpty())
@@ -48,7 +56,7 @@ void Queue::enqueue(int num)
 	num_items++;
 }
 
-void Queue::dequeue(int &x)
+void Queue::dequeue(int **move, int **root_move, int &row, int &col)
 {
 	node *temp = nullptr;
 
@@ -58,7 +66,18 @@ void Queue::dequeue(int &x)
 	}
 	else
 	{
-		x = front->value;
+		for (int i = 0; i < 3; i++)
+		{
+			memcpy(*(move+i), *(front->move + i), sizeof(*move));
+		}
+		for (int i = 0; i < 3; i++)
+		{
+			memcpy(*(root_move + i), *(front->root_move + i), sizeof(*root_move));
+		}
+		
+		row = front->row;
+		col = front->col;
+
 		temp = front;
 		front = front->next;
 		delete temp;
@@ -68,6 +87,24 @@ void Queue::dequeue(int &x)
 	}
 }
 
+void Queue::dequeue()
+{
+	node *temp = nullptr;
+
+	if (isEmpty())
+	{
+		cout << "Queue is empty!\n";
+	}
+	else
+	{
+		temp = front;
+		front = front->next;
+		delete temp;
+
+		num_items--;
+
+	}
+}
 bool Queue::isEmpty()
 {
 	if (num_items > 0)
@@ -78,9 +115,35 @@ bool Queue::isEmpty()
 
 void Queue::clear()
 {
-	int value;
-
 	while (!isEmpty())
-		dequeue(value);
+		dequeue();
 }
 
+bool Queue::contains(int move[3][3])
+{
+	node *temp;
+
+	temp = front;
+	for (int i = 0; i < num_items; i++)
+	{
+		if (compare(temp->move, move))
+			return true;
+
+		temp = temp->next;
+	}
+	return false;
+}
+
+bool Queue::compare(int move[3][3], int move2[3][3])
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			if (move[i][j] != move2[i][j])
+				return false;
+		}
+	}
+
+	return true;
+}
