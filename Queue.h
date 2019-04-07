bcb9de2ea60bd4c96dd48c51df0ee6ef7111d2a4
dvilajeti01 <43216832+dvilajeti01@users.noreply.github.com>
2019-04-06 20:18:11 -0400

    Update Queue.h

--- Queue.h
+++ Queue.h
@@ -5,7 +5,7 @@
 //  AI_Project1
 //
 //  Created by Daniel Vilajeti on 3/3/19.
-//  Copyright © 2019 Daniel Vilajeti. All rights reserved.
+//  Copyright  2019 Daniel Vilajeti. All rights reserved.
 //
 
 
@@ -14,8 +14,13 @@ class Queue
 private:
 	struct node
 	{
-		int value;
+		int move[3][3];
+		int root_move[3][3];
 		node *next;
+
+		//location of zero
+		int row;
+		int col;
 	};
 
 	node *front;
@@ -28,15 +33,18 @@ public:
 
 	~Queue();
 
-	void enqueue(int num);
+	void enqueue(int move[3][3], int **root_move, int row, int col);
 
-	void dequeue(int &x);
+	void dequeue(int **move, int **root_move, int &row, int &col);
+
+	void dequeue();
 
 	bool isEmpty();
 
 	void clear();
 
+	bool contains(int move[3][3]);
+
+	bool compare(int move[3][3], int move2[3][3]);
 
 };
-
-
