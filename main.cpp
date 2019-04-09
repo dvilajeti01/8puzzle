20788251e85aecd985c40d27a72ff0f1092e21fc
dvilajeti01 <43216832+dvilajeti01@users.noreply.github.com>
2019-04-08 20:51:08 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,25 @@
-
 #include <iostream>
 #include <unordered_map>
 #include "Puzzle.h"
 #include "Queue.h"
+#include <chrono>
 using namespace std;
 
 
 int main(int argc, const char * argv[]) {
-    
-    using namespace chrono;
-    int arr[3][3] = {{2,0,5},{1,3,6},{7,4,8}};
-    Puzzle test(arr);
-   
-    high_resolution_clock::time_point start_T = high_resolution_clock::now();
-    test.solve();
-    high_resolution_clock::time_point end_T = high_resolution_clock::now();
-    
-    cout << "The progrma took " << duration_cast<seconds>(end_T - start_T).count() << " seconds." << endl;
-    
-    system("pause");
-    
-    return 0;
+
+	using namespace chrono;
+
+	int arr[3][3] = { { 2,0,5 },{ 1,3,6 },{ 7,4,8 } };
+	Puzzle test(arr);
+
+	high_resolution_clock::time_point start_T = high_resolution_clock::now();
+	test.solve();
+	high_resolution_clock::time_point end_T = high_resolution_clock::now();
+
+	cout << "The program took " << duration_cast<milliseconds>(end_T - start_T).count() << " milliseconds." << endl;
+
+	system("pause");
+
+	return 0;
 }
