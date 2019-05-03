aa866b15bd31cc3c62750ec5e8302a22a5c29e9c
dvilajeti01 <dvilajeti01@manhattan.edu>
2019-05-02 20:15:53 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <unordered_map>
 #include "Puzzle.h"
@@ -7,30 +8,53 @@ using namespace std;
 
 
 int main(int argc, const char * argv[]) {
-    
-    using namespace chrono;
-    
-    int arr[3][3] = { { 2,0,5 },{ 1,3,6 },{ 7,4,8 } };
-    
-    int arr2[3][3] = { {8,0,6}, {5,4,7}, {2,3,1} };
-    Puzzle test(arr);
-    
-    high_resolution_clock::time_point start_T = high_resolution_clock::now();
-    test.solve("BFS");
-    high_resolution_clock::time_point end_T = high_resolution_clock::now();
-    
-    cout << "The program took " << duration_cast<milliseconds>(end_T - start_T).count() << " milliseconds." << endl;
-    
-    system("pause");
-    
-    Puzzle test2(arr);
-    start_T = high_resolution_clock::now();
-    test2.solve("A_Star");
-    end_T = high_resolution_clock::now();
-    
-    cout << "The program took " << duration_cast<milliseconds>(end_T - start_T).count() << " milliseconds." << endl;
-    //cout << test.getCumulativeCost() << endl;
-    system("pause");
-    
-    return 0;
+
+	using namespace chrono;
+
+	int arr[3][3] = { { 2,0,5 },{ 1,3,6 },{ 7,4,8 } };
+	int arr2[3][3] = { {8,0,6}, {5,4,7}, {2,3,1} };
+	Puzzle test(arr);
+
+	high_resolution_clock::time_point start_T = high_resolution_clock::now();
+	test.solve("BFS");
+	high_resolution_clock::time_point end_T = high_resolution_clock::now();
+
+	cout << "The program took " << duration_cast<milliseconds>(end_T - start_T).count() << " milliseconds." << endl;
+
+	system("pause");
+
+	Puzzle test2(arr2);
+
+	start_T = high_resolution_clock::now();
+	test2.solve("BFS");
+	end_T = high_resolution_clock::now();
+
+	cout << "The program took " << duration_cast<milliseconds>(end_T - start_T).count() << " milliseconds." << endl;
+	//cout << test.getCumulativeCost() << endl;
+	system("pause");
+
+	Puzzle test3(arr);
+
+	start_T = high_resolution_clock::now();
+	test3.solve("A_Star");
+	end_T = high_resolution_clock::now();
+
+	cout << "The program took " << duration_cast<milliseconds>(end_T - start_T).count() << " milliseconds." << endl;
+	//cout << test.getCumulativeCost() << endl;
+	system("pause");
+
+	Puzzle test4(arr2);
+
+	start_T = high_resolution_clock::now();
+	test4.solve("A_Star");
+	end_T = high_resolution_clock::now();
+
+	cout << "The program took " << duration_cast<milliseconds>(end_T - start_T).count() << " milliseconds." << endl;
+	//cout << test.getCumulativeCost() << endl;
+	system("pause");
+
+	return 0;
 }
+
+
+
